ee0897ac454c0dfe554e89f22fffd44ac291c025
zc110747 <1107473010@qq.com>
2020-06-05 16:15:24 +0800

    -upper_app add

--- upper_app/manage_tool/udpclient.h
+++ upper_app/manage_tool/udpclient.h
@@ -1,4 +1,12 @@
 #ifndef UDPCLIENT_H
 #define UDPCLIENT_H
 
+#include <QObject>
+#include <QThread>
+#include <QTcpSocket>
+#include <QHostAddress>
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "protocol.h"
+
 #endif // UDPCLIENT_H
