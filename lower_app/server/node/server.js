3549312545ac5e03613e68b0c103dcb8b7eaeff4
JasonZhang <jason.zhang@carestream.com>
2022-08-04 17:31:28 +0800

    update for node sever, use ts.

--- lower_app/server/node/server.js
+++ lower_app/server/node/server.js
@@ -1,63 +1,47 @@
 //module
-const http = require('http');
-const fs = require('fs');
-const { extname, join } = require('path');
-
+var http = require('http');
+var fs = require('fs');
+var _a = require('path'), extname = _a.extname, join = _a.join;
 //global parameter
-const server = http.createServer();
-let netInfo = {
-    ipaddr:'127.0.0.1',
-    port:8080
+var server = http.createServer();
+var netInfo = {
+    ipaddr: '127.0.0.1',
+    port: 8080
 };
-let contentType = {
-    "html":"text/html",
-    "xml":"text/xml",
-    "asp":"text/asp",
-    "jpg":"image/jpeg",
-    "png":"image/png",
-    "ico":"image/icon",
-    "js":"application/x-javascript",
+var contentType = {
+    "html": "text/html",
+    "xml": "text/xml",
+    "asp": "text/asp",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "ico": "image/icon",
+    "js": "application/x-javascript"
 };
-
-let localpath = `${__dirname}\\web\\`;
-
-function hasOwnKey(obj, key)
-{
+var localpath = "".concat(__dirname, "\\web\\");
+function hasOwnKey(obj, key) {
     return (key in obj);
 }
-console.log(hasOwnKey(contentType, "ico"));
-
-function main()
-{
-    server.on('request', (request, response)=>{
-        //request do
-        let pathname = request.url;
-        pathname = pathname==='/'? 'index.html':pathname;
-        let filepath = join(localpath, pathname);
-        if(fs.existsSync(filepath))
-        {
-            const ext = extname(filepath).slice(1);
-    
-            if(hasOwnKey(contentType, ext))
-                response.setHeader('content-type', contentType[ext]);
-            else
-                console.log(`content-type:${ext}`);
-    
-            let html;
-            html = fs.readFileSync(filepath);
-            response.end(html);
-        }
+server.on('request', function (request, response) {
+    //request do
+    var pathname = request.url;
+    pathname = pathname === '/' ? 'index.html' : pathname;
+    var filepath = join(localpath, pathname);
+    if (fs.existsSync(filepath)) {
+        var ext = extname(filepath).slice(1);
+        if (hasOwnKey(contentType, ext))
+            response.setHeader('content-type', contentType[ext]);
         else
-        {
-            console.log(`invalid pathname ${pathname}`);
-            response.statusCode = 404;
-            response.end("not found");
-        }
-    });
-
-    server.listen(netInfo.port, netInfo.ipaddr, ()=>{
-        console.log(`server listen ip:${netInfo.ipaddr}, port ${netInfo.port}`)
-    });
-}
-
-main()
+            console.log("content-type:".concat(ext));
+        var html = void 0;
+        html = fs.readFileSync(filepath);
+        response.end(html);
+    }
+    else {
+        console.log("invalid pathname ".concat(pathname));
+        response.statusCode = 404;
+        response.end("not found");
+    }
+});
+server.listen(netInfo.port, netInfo.ipaddr, function () {
+    console.log("server listen ip:".concat(netInfo.ipaddr, ", port ").concat(netInfo.port));
+});
