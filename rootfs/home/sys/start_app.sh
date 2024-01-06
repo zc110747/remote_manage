0f9305afbe2e786ad4111b8f8a17731e6a88952d
jason <1107473010@qq.com>
2024-01-06 08:19:04 +0800

    update support busybox and debain.

--- rootfs/home/sys/start_app.sh
+++ rootfs/home/sys/start_app.sh
@@ -1,15 +1,9 @@
-#/usr/bin/bash
 
-app_list=("node server.js" "main_process" "logger_tool" "local_device" "mosquitto")
-
-for app in "${app_list[@]}"; do
-    run_id=$(ps aux | grep '${app}' | grep -v grep | awk '{print $2}')
-    echo "${app}, id:${run_id}"
-
-    if [ ! -z ${run_id} ]; then
-        kill -9 ${run_id}
-    fi
-done
+#stop all device
+pkill -9 node
+pkill -9 logger_tool
+pkill -9 local_device
+pkill -9 main_process
 
 if [ ! -d /tmp/app ]; then
     mkdir -p /tmp/app
